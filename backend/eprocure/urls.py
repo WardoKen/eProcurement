dccf50f95ef0fc29b4052812cb252cefcebb31ed
Howard Kenneth Bereber <bereberhowardkenneth@gmail.com>
2026-08-07 11:03:53 +0800

    Enable Django admin

--- backend/eprocure/urls.py
+++ backend/eprocure/urls.py
@@ -1,16 +1,18 @@
 from pathlib import Path
 
 from django.conf import settings
+from django.contrib import admin
 from django.urls import include, path
 from django.views.static import serve
 
 urlpatterns = [
-    path('api/', include('api.urls')),
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),
     path(
-        'uploads/<path:path>',
+        "uploads/<path:path>",
         serve,
         {
-            'document_root': str(Path(settings.BASE_DIR) / 'uploads'),
+            "document_root": str(Path(settings.BASE_DIR) / "uploads"),
         },
     ),
 ]
